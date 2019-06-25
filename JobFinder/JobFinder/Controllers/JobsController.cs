3233f66edf37390dcd2bf8f8a5d04977b4d5128b
IvanNenov <nenow@abv.bg>
2019-06-25 11:15:17 +0300

    Add post a job page

---
+++ JobFinder/JobFinder/Controllers/JobsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobFinder.Controllers
+{
+    public class JobsController : Controller
+    {
+        public IActionResult PostJob()
+        {
+            return this.View();
+        }
+
+        //public async Task<IActionResult> PostJob(PostJobInputModel model)
+        //{
+
+        //}
+    }
+}
