a89872f80480b8ed90406f0b80f73b526bb8518b
IvanNenov <nenow@abv.bg>
2019-08-18 18:21:05 +0300

    remove a href from company name and add required att in form entry

--- JobFinder/JobFinder.ViewModels/InputViewModels/FormEntryInputViewModel.cs
+++ JobFinder/JobFinder.ViewModels/InputViewModels/FormEntryInputViewModel.cs
@@ -8,15 +8,18 @@ namespace JobFinder.ViewModels.InputViewModels
     public class FormEntryInputViewModel
     {
         [MaxLength(50)]
+        [Required]
         public string SenderName { get; set; }
 
         [EmailAddress]
+        [Required]
         public string SenderEmail { get; set; }
 
         [MaxLength(10)]
         public string PhoneNumber { get; set; }
 
         [MaxLength(100)]
+        [Required]
         public string Message { get; set; }
 
         public DateTime CreatedOn { get; set; }
